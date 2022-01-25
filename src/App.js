293cf897215340eeadb4ab71abe58a58b727505e
Gursimran9 <simrangur12345@gmail.com>
2022-01-24 23:10:03 -0500

    useEffect hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [width, setWidth] = useState(window.innerWidth);
+
+  // I will render everytime
+  useEffect(() => {
+    console.log('I render everytime');
+  });
+
+  // I will render when the component mounts
+  useEffect(() => {
+    console.log('I render when Component mounts');
+  }, []);
+
+  // I will render when component mounts and whenever dependency changes
+  useEffect(() => {
+    console.log(`The name is Changed :${name}`);
+  }, [name]);
+
+  //It will be used when want to unmount a component after it has been trigger
+  useEffect(() => {
+    window.addEventListener('resize', updateWidth);
+
+    return () => {
+      window.removeEventListener('resize', updateWidth);
+    };
+  });
+
+  const updateWidth = () => {
+    setWidth(window.innerWidth);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>The width is: {width}</h1>
+      <form>
+        <input
+          type='text'
+          placeholder='Enter name...'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type='text'
+          placeholder='Enter age...'
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />
+      </form>
     </div>
   );
 }
